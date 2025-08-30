b3479665bbda56fe31659bc72a321ced08734597
Pathan Islam Hussain <islamhussain.pathan@gmail.com>
2025-08-30 11:41:48 +0530

    Configure Keycloak to use same PostgreSQL database as eShop
    - Update Keycloak to use eshop database instead of separate keycloak database
    - Configure Keycloak to use 'keycloak' schema within eshop database
    - Update database initialization script to create keycloak schema
    - Remove separate keycloak database creation
    - Both eShop and Keycloak now share the same PostgreSQL instance
    - Reduces resource usage and simplifies database management
    - Keycloak tables are properly isolated in 'keycloak' schema
    - eShop tables remain in 'catalog' and 'public' schemas

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -1,16 +1,16 @@
 -- Initialize eShop database
 -- This script runs when PostgreSQL container starts
 
--- Create Keycloak database
-CREATE DATABASE keycloak;
-
 -- Create test database
 CREATE DATABASE eshop_test;
 
 -- Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE eshop TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE keycloak TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE eshop_test TO postgres;
+
+-- Create Keycloak schema in eshop database
+\c eshop;
+CREATE SCHEMA IF NOT EXISTS keycloak;
 
 -- Create extensions if needed
 \c eshop;
